d2c8f43eb83f1f55b3898245a5e17f75e79c24fa
Misterjo <plarocque.joel@gmail.com>
2021-07-14 13:51:32 -0400

    removed unused and unsupported methods

--- src/database/Database.js
+++ src/database/Database.js
@@ -20,45 +20,11 @@ class Database {
         });
     }
 
-
-    static async fetchUserWeatherConfig(id){
-        let user = await model.User.findOne({
-            discordId : id
-        });
-        return user.weather
-    }
-
-    static async addSubreddit(link){
-        let subreddit = await model.Subreddit.findOne({link: link});
-        if(!subreddit){
-            // TODO FETCH DATA FROM REDDIT API
-            subreddit = new model.Subreddit({
-                link:link,
-                name: 'defaultName'
-            });
-        }
-        return subreddit
-    }
-
-    static async addSubredditToUser(UserID,link){
-        //IF subreddit doesnt exist, create subreddit and add subredditID to user.subreddit_ids
-        //IF subreddit exists, add the subredditID to user.subreddit_ids
-
-    }
-
     static async findUser(discordID) {
         return model.User.findOne({
             discordId: discordID
         }).exec();
     }
-
-    static async getUserSubreddits(discordID){
-        let user = await findUser(discordID);
-        let subreddit_ids = user.subreddit_ids;
-        let subreddits = await model.Subreddit.find({ '_id': { $in: subreddit_ids } });
-        console.log(subreddits);
-    }
-
 }
 
 module.exports = Database;
